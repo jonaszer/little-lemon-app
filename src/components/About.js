6b116566706bf63b9907b5d27e692757e451570e
Jonas Zeringis <j.zeringis@yahoo.com>
2023-08-08 22:57:19 +0100

    Added About sections, together with responsivness across variety of device sizes

---
+++ src/components/About.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../components/css/About.css';
+import MarioAdrian1 from '../assets/marioadrian1.jpg';
+import MarioAdrian2 from '../assets/marioadrian2.jpg';
+
+function About() {
+  return (
+    <>
+        <div className="about-container">
+            <div className="photos-description-container">
+                <div className="title-container">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                </div>
+                <div className="photo-paragraph-container">
+                    <div className="both-photos-container">
+                        <div className='photo-left'>
+                            <img className='photo' src={MarioAdrian2} alt='Mario and Adrian.'/>
+                        </div>
+                        <div className='photo-right'>
+                            <img className='photo border' src={MarioAdrian1} alt='Mario and Adrian.'/>
+                        </div>
+                    </div>
+                    <div className='paragraph-container'>
+                        <p>Lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon lemon.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default About;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Highlights from '../components/Highlights.js';
 import Testimonials from '../components/Testimonials.js';
+import About from '../components/About.js';
 
 function Main() {
   return (
     <>
       <Highlights/>
       <Testimonials/>
+      <About/>
     </>
   )
 }
